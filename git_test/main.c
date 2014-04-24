e18060de1a2c61a4247c5e1c065400a9dd669962
chrooot <ggdsdhjj@yahoo.co.jp>
2014-04-24 18:10:31 +0900

    Initial Commit

---
+++ git_test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  git_test
+//
+//  Created by s.k on 2014/04/24.
+//  Copyright (c) 2014年 chroot. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
